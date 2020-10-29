e9c042502bf213e9cb3d552e376f151f78e07609
Thanhdat1905 <thanhdat19051998@gmail.com>
2020-10-29 21:45:23 +0700

    food Website

---
+++ foodWebsite/store/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def store(request):
+    context = {}
+    return render(request, 'store/index.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
+
+
+def contact(request):
+    context = {}
+    return render(request, 'store/contact.html', context)
+
+
+def blog(request):
+    context = {}
+    return render(request, 'store/blog.html', context)
